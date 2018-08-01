b89b71e93416fd7a6071d6e2dcfd8c1764380515
vagrant <vagrant@homestead>
2018-08-01 06:19:53 +0000

    管理后台 -新增，修改，删除

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -10,6 +10,7 @@ use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Content;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
+use Illuminate\Validation\Rule;
 
 class CouponCodesController extends Controller
 {
@@ -39,7 +40,7 @@ class CouponCodesController extends Controller
     {
         return Admin::content(function (Content $content) use ($id) {
 
-            $content->header('header');
+            $content->header('编辑优惠券');
             $content->description('description');
 
             $content->body($this->form()->edit($id));
@@ -55,7 +56,7 @@ class CouponCodesController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('header');
+            $content->header('新增优惠券');
             $content->description('description');
 
             $content->body($this->form());
@@ -113,9 +114,43 @@ class CouponCodesController extends Controller
         return Admin::form(CouponCode::class, function (Form $form) {
 
             $form->display('id', 'ID');
+            $form->text('name', '名称')->rules('required');
+            $form->text('code','优惠码')->rules(function ($form) {
+                // 如果 $form->model()->id  不为空，代表是编辑操作
+                if ($id = $form->model()->id) {
+                   // return 'nullable|unique:coupon_codes,id,'.$id;
+                    return [
+                        'nullable',
+                        Rule::unique('coupon_codes')->ignore($id),
+                        ];
+                } else {
+                    return 'nullable|unique:coupon_codes';
+                }
+            });
+            $form->radio('type', '类型')->options(CouponCode::$typeMap)->rules('required');
+            $form->text('value', '折扣')->rules(function ($form) {
+                // 如果选择了百分比折扣类型，那么折扣范围只能是 1 ~ 99
+                if ($form->type === CouponCode::TYPE_PERCENT) {
+                    return 'required|numeric|between:1,99';
+                } else {
+                    // 否则只要大于等于 0.01 即可
+                    return 'required|numeric|min:0.01';
+                }
+            });
 
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
+            $form->text('total', '总量')->rules('required|numeric|min:0');
+            $form->text('min_amount', '最低金额')->rules('required|numeric|min:0');
+            $form->datetime('not_before', '开始时间');
+            $form->datetime('not_after', '结束时间');
+            $form->radio('enabled', '启用')->options(['1' => '是', '0' => '否']);
+
+
+            $form->saving(function (Form $form) {
+
+                if (!$form->code) {
+                    $form->code = CouponCode::findAvailableCode();
+                }
+            });
         });
     }
 }
